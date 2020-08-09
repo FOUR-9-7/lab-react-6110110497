a72b8127a49b001ecc53f90539b409cd7befec45
FOUR-9-7 <jafharo@gmail.com>
2020-08-09 18:34:20 +0700

    Hello

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import CharacterCard from './CharacterCard';
 import logo from './logo.svg';
 import './App.css';
-
+const word = "Hello";
 function App() {
-  return (
-    <div >
-     Hello World
-    </div>
-  );
+ return (
+ <div>
+ {
+ Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+ }
+ </div>
+ );
 }
+
 
 export default App;
