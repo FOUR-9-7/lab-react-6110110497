81e020e46f89db8ab77cc159d7867921c993cf60
FOUR-9-7 <jafharo@gmail.com>
2020-08-09 18:45:39 +0700

    add wordcard // test 4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 import logo from './logo.svg';
 import './App.css';
 const word = "Hello";
 function App() {
- return (
- <div>
- {
- Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
- }
- </div>
- );
+  return (
+    <div>
+    <WordCard value="hello"/>
+    </div>
+    );
 }
 
 
